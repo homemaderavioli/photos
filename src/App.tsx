293d92936ff72c52fb713c1fa58e98d33704be8a
freewalk <fr33w4lk@gmail.com>
2020-07-01 01:15:12 +1000

    initial photo website

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+type PhotoProps = {
+  url: string;
+  alt: string;
+};
+
+class Photo extends React.PureComponent<PhotoProps> {
+  render = () => (
+    <div className="Photo"><img src={this.props.url} alt={this.props.alt}></img></div>
+  );
+}
+
+type PhotoGridProps = {
+  initialIndex: number,
+  photos: string[],
+};
+
+type PhotoGridState = {
+  photoIndex: number,
+};
+
+class PhotoGrid extends React.Component<PhotoGridProps, PhotoGridState> {
+  state: PhotoGridState = {
+    photoIndex: this.props.initialIndex,
+  };
+
+  render = () => {
+    return (
+      <div className="PhotoGrid">
+        <Photo url={this.props.photos[this.state.photoIndex]} alt="alt" />
+        <div className="prev" onClick={() => this.prev(this.props.photos.length)}></div>
+        <div className="next" onClick={() => this.next(this.props.photos.length)}></div>
+      </div>
+    );
+  }
+
+  next = (len: number): void => {
+    this.setState((state => ({
+      photoIndex: (state.photoIndex + 1) % len,
+    })));
+  };
+
+  prev = (len: number): void => {
+    this.setState((state => ({
+      photoIndex: state.photoIndex - 1 < 0 ? (len - 1) : state.photoIndex - 1,
+    })));
+  };
+}
+
+const App = () => {
+  const photos:string[] = [
+    "pics/DSC_0668.jpg",
+    "pics/DSC_0553.jpg",
+    "pics/DSC_0549.jpg",
+    "pics/DSC_0384.jpg",
+    "pics/darling-harbour.jpg",
+    "pics/door.jpg",
+    "pics/DSC_0016.jpg",
+  ];
+  const intitialIndex = Math.floor(Math.random() * Math.floor(photos.length))
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title"><h1>photos</h1></div>
+        <div className="menu"><ul><li></li></ul></div>
       </header>
+      <PhotoGrid
+        initialIndex={intitialIndex}
+        photos={photos}
+      />
     </div>
   );
 }
