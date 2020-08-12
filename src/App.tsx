9bd448fb22a4815c415ee17ba02c2afc573efd0c
Ravel Ford <ravel.ford@protonmail.com>
2020-08-13 00:05:22 +1000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PhotoGrid from './Photo'
 
 const App = () => {
   const BASE_IMAGES_URL = `${process.env.PUBLIC_URL}/assets/images`
+  console.log(require.context(BASE_IMAGES_URL, false, /\.(jp?eg)$/));
   const PHOTOS:string[] = [
     `${BASE_IMAGES_URL}/DSC_0668.jpg`,
     `${BASE_IMAGES_URL}/DSC_0553.jpg`,
